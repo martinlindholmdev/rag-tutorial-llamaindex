0109215f9f868dc12f1ad745e698829f732d1ec7
Martin L <martin.lindholm.dev@gmail.com>
2025-11-26 16:38:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ nest_asyncio.apply()
 
 import streamlit as st
 import os
-7   from llama_index.core import StorageContext, load_index_from_storage
-17
-18  if "OPENAI_API_KEY" in st.secrets:
+from llama_index.core import StorageContext, load_index_from_storage
+
+if "OPENAI_API_KEY" in st.secrets:
     os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
 SYSTEM_PROMPT = """Du är en varm och erfaren förälder som själv har barn med NPF. Du har läst boken "Pusselfamiljens verktyg för vardagshjältar" många gånger och delar gärna med dig av tipsen på ett personligt sätt.
